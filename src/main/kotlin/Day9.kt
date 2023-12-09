38846d43e201a617aff8c43ec7cc50cedb451884
Andreas Toth <me@ondr.sh>
2023-12-09 08:30:09 +0200

    Solve day 9

---
+++ src/main/kotlin/Day9.kt
@@ -0,0 +1,14 @@
+import java.io.File
+
+fun main() {
+    val lines = File("input/9.txt").readLines().map { it.split(" ").map { it.toInt() } }
+
+    fun List<Int>.extrapolate(): Int = if (all { it == 0 }) 0 else {
+        last() + windowed(2) { it.last() - it.first() }.extrapolate()
+    }
+
+    val ans1 = lines.sumOf { it.reversed().extrapolate() }
+    val ans2 = lines.sumOf { it.extrapolate() }
+    println(ans1)
+    println(ans2)
+}
